096ef007b9bfccdf2ba81f4215e0cef56e510f76
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2018-10-18 01:38:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,25 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[5];
+    int str[100],i=0,count=0;
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    while(1){
+        scanf("%d",&str[i]);
+        i++;
+        count++;
+        if(str[i]==0)
+            break;
+        else if(str[i]==-1)
+            break;
+    }
+    if(str[i]==-1){
+        for(i=0;i<count-1;i++){
+            printf("%d",str[i]);
+        }
+    }
+    
+    else{
+        printf("%d",str[i]);
     }
 
     return 0;
