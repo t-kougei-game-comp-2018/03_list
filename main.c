cb53796f420d53764baf5c37a56aa9e838271852
kireha <43433438+kireha@users.noreply.github.com>
2018-10-21 14:44:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    char a[100];
+    int i=0;
     
     while(fgets(str, sizeof(str), stdin)){
+        if()
         printf("%s", str);
     }
 
