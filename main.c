1cc979dac5dd828e7f8be54e646fc689a8b52c0f
Ytt-yusuke <43527062+Ytt-yusuke@users.noreply.github.com>
2018-10-21 10:31:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,50 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[5];
+    char str[100];
+    int array[100],i = 0,j;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int input = atoi(str);
+        
+        if(input > 0 && i = 0)
+        {
+            array[i] = input;
+            i++;
+        }
+        else if(input > 0 && i > 0)
+        {
+            for(j = 0; j < i; j++)
+            {
+                array[j + 1] = array[j];
+            }
+            
+            array[0] = input;
+            i++;
+        }
+        else if(input == 0)
+        {
+            for(j = 0; j < i; j++)
+            {
+                printf("%d",array[j]);
+                
+                if(j < i - 1)
+                {
+                    printf(",");
+                }
+            }
+        }
+        else if(input == -1)
+        {
+            printf("%d",array[0]);
+            
+            for(j = 0; j < i - 1; j++)
+            {
+                array[j] = array[j + 1];
+            }
+            
+            i--;
+        }
     }
 
     return 0;
