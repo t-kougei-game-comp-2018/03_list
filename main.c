2358ef5793195b8f424370bb314e8b58e6500378
kireha <43433438+kireha@users.noreply.github.com>
2018-10-22 09:41:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -11,14 +11,13 @@ int main(int argc, char *argv[])
             i++;
         }
         if(x==0){
-            printf("%c",a[i-1]);
+            printf("%c",a[i]);
             for(j=i-1;j>0;j--)
                 printf(",%c",a[j]);
             printf("\n");
         }
         if(x==-1){
             printf("%c\n",a[i-1]);
-            a[i-1]='\0';
             i--;
         }
     }
